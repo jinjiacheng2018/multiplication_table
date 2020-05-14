fc7831ec8d240114d7356f25eb63a4daa8dfe769
jinjiacheng2018 <1935922807@qq.com>
2020-05-14 13:07:55 +0800

    上传文件

--- Mulitip.java
+++ Mulitip.java
@@ -1,7 +1,7 @@
 public class Mulitip{
 	
 	public static void main(String[] args){
-		
+		// 打印九九乘法表
 		for (int i = 1; i < 10; i++){
 			for (int j = 1; j <= i; j++){
 				System.out.print(j + "*" + i + "=" + (i*j) + " ");
